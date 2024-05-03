8cd5933a4fd75e552788d90505a50769851b1ac1
jayphan14 <kha21052004@gmail.com>
2024-05-03 13:22:30 -0400

    create handlers

--- main.go
+++ main.go
@@ -3,11 +3,35 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/nats-io/nats.go"
+	"linkedforlinked.com/user/userhandlers"
 )
 
 func main() {
+	// Connect to NATS server
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		fmt.Printf("Error connecting to NATS server: %v\n", err)
+		return
+	}
+	defer nc.Close()
+
+	subjectHandlers := map[string]func(msg *nats.Msg){
+		"user.create": userhandlers.HandleUserCreate,
+		"user.delete": userhandlers.HandleUserDelete,
+	}
+
+	for subject, handler := range subjectHandlers {
+		sub, err := nc.Subscribe(subject, handler)
+		if err != nil {
+			fmt.Printf("Error subscribing to subject: %v\n", err)
+			return
+		}
+		defer sub.Unsubscribe()
+	}
+
 	for {
-		fmt.Println("Hello World")
 		time.Sleep(1 * time.Second)
 	}
 }
